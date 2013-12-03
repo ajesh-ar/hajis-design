00e20d3c531b2628ffaa4971eb7de0da40043c6f
ar <akhilajesh@gmail.com>
2013-12-03 14:17:02 +0530

    First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hajis::Application.config.secret_token = '894291d2df00a763e472882c7900195ee25d8b4e40d8e05249d1d0221b4f25f555a17d8dc43bba5a9b7b929ba886c03dc5fd99ec42605e18e23e5f58780580f8'
